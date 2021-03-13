3df22e1e68c767bd8cccbec54a92adf5741de957
JooYae <jooyae3970@naver.com>
2021-03-13 14:42:27 +0900

    map view

--- app/src/main/java/com/example/kakaomap/MainActivity.kt
+++ app/src/main/java/com/example/kakaomap/MainActivity.kt
@@ -9,42 +9,54 @@ import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import com.example.kakaomap.databinding.ActivityMainBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-
         val mapView = MapView(this)
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.5514579595, 126.951949155),true)
+        mapView.setZoomLevel(7,true)
         val mapViewContainer = binding.mapView
         mapViewContainer.addView(mapView)
-
+        setMarker()
         getHashKey()
     }
 
-    fun getHashKey(){
-        var packageInfo : PackageInfo = PackageInfo()
-        try{
-            packageInfo = packageManager.getPackageInfo(packageName,PackageManager.GET_SIGNATURES )
-        }catch (e:PackageManager.NameNotFoundException){
+    fun getHashKey() {
+        var packageInfo: PackageInfo = PackageInfo()
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
-        for(signature : Signature in packageInfo.signatures)
-        {
-            try{
-                var md : MessageDigest = MessageDigest.getInstance("SHA")
+        for (signature: Signature in packageInfo.signatures) {
+            try {
+                var md: MessageDigest = MessageDigest.getInstance("SHA")
                 md.update(signature.toByteArray())
-                Log.e("KEY_HASH", Base64.encodeToString(md.digest(),Base64.DEFAULT))
-            }catch(e: NoSuchAlgorithmException){
-                Log.e("KEY_HASH", "Unable to get MessageDigest.signature = "+ signature,e)
+                Log.e("KEY_HASH", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KEY_HASH", "Unable to get MessageDigest.signature = " + signature, e)
             }
 
         }
     }
+
+    fun setMarker() {
+        val marker = MapPOIItem()
+        val mapView = MapView(this)
+        marker.itemName = "주예"
+        marker.tag = 0
+        marker.markerType = MapPOIItem.MarkerType.YellowPin
+        mapView.addPOIItem(marker)
+    }
+
 }
